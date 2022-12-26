ae0bbd4edf6c59f4af9343e20fbe79c4176e4293
lvke <lvke>
2022-12-26 23:25:25 +0800

    add glad

--- learnopengl/src/main.cpp
+++ learnopengl/src/main.cpp
@@ -1,4 +1,18 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+
+void KeyCallBack(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE)
+		glfwSetWindowShouldClose(window, true);
+}
+
+void OnResize(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
 
 int main(void)
 {
@@ -15,30 +29,36 @@ int main(void)
 		glfwTerminate();
 		return -1;
 	}
-
+	//gladLoadGL();
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, OnResize);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		//std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
+		glfwSetKeyCallback(window, KeyCallBack);
+
 		/* Render here */
+		glClearColor(0.5f, 0.5f, 0.0f, 0.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, - 0.5f);
-		glVertex2f(0.5f, - 0.5f);
-		glVertex2f(0.0f, 0.5f);
-		glEnd();
-
-
+		
+		
+		//glGenBuffers()
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
 
 		/* Poll for and process events */
 		glfwPollEvents();
-	}
+	} 
 
 	glfwTerminate();
 	return 0;
